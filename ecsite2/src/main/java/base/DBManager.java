928273b7d69a32b01d04e45ac9db8cbf225cadd0
添田亮司 <soeda-r@groove-gear.jp>
2019-08-13 19:05:41 +0900

    postgres調整

--- ecsite2/src/main/java/base/DBManager.java
+++ ecsite2/src/main/java/base/DBManager.java
@@ -14,7 +14,7 @@ public class DBManager {
 //            Class.forName("com.mysql.cj.jdbc.Driver");
 //            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/kagoyume_db?serverTimezone=JST","root","");
         	  Class.forName("org.postgresql.Driver");
-        	  con = DriverManager.getConnection("jdbc:postgres://vpziupvssvzqsp:6e35c9eb8da9b28bf1aaefc077a3abab280c6d1080fad27ae4a74f09766b6f18@ec2-54-243-47-196.compute-1.amazonaws.com:5432/dd1e4gm97d2md4", "vpziupvssvzqsp", "6e35c9eb8da9b28bf1aaefc077a3abab280c6d1080fad27ae4a74f09766b6f18");
+        	  con = DriverManager.getConnection("jdbc:postgres://ec2-54-243-47-196.compute-1.amazonaws.com:5432/dd1e4gm97d2md4?sslmode=require", "vpziupvssvzqsp", "6e35c9eb8da9b28bf1aaefc077a3abab280c6d1080fad27ae4a74f09766b6f18");
             System.out.println("DBConnected!!");
             return con;
         }catch(ClassNotFoundException e){
